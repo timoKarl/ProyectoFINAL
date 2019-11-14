253b087ad381ea707cc3c55cc1dc2dee4bc01777
ian <ijamui@udesa.edu.ar>
2019-11-14 14:53:00 -0300

    efv

--- detalle.js
+++ detalle.js
@@ -4,7 +4,6 @@ window.onload = ()=>{
   fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US")
   .then(res => res.json())
   .then(data => {
-  console.log(data);
   var generos = data.genres
   for (var i = 0; i < generos.length; i++) {
     var nombre = generos[i].name
@@ -21,7 +20,6 @@ window.onload = ()=>{
   fetch("https://api.themoviedb.org/3/tv/"+ idSerie +"?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US")
   .then(res => res.json())
   .then(data => {
-    console.log(data);
     var nombre = data.name;
     var prepath = "https://image.tmdb.org/t/p/original"
     var path = data.poster_path
@@ -52,9 +50,13 @@ window.onload = ()=>{
     var ulEpisodio = data.last_episode_to_air.name
     var fechaUlEpisodio = data.last_episode_to_air.air_date
     document.querySelector('#ulEpisodio').innerHTML += ulEpisodio + " emitido el " + fechaUlEpisodio
+    if (data.next_episode_to_air == null) {
+      document.querySelector('#proxEpisodio').style.display = "none";
+    }else {
     var proxEpisodio = data.next_episode_to_air.name
     var fechaProxEpisodio = data.next_episode_to_air.air_date
     document.querySelector('#proxEpisodio').innerHTML += proxEpisodio + " el " + fechaProxEpisodio
+  }
     var temporadas = data.number_of_seasons
     document.querySelector('#temporadas').innerHTML += temporadas
     var episodios = data.number_of_episodes
@@ -71,8 +73,71 @@ var videoLink = ""
 for (var i = 0; i < videoResults.length; i++) {
   videoNombre = videoResults[i].name
   videoLink = videoResults[i].key
-  document.querySelector('#videosTodos').innerHTML += '<a class="uk-button uk-button-default" href="#modal-media-youtube" uk-toggle><p id="videoNombre">'+ videoNombre +'</p></a><div id="modal-media-youtube" class="uk-flex-top" uk-modal><div class="uk-modal-dialog uk-width-auto uk-margin-auto-vertical"><button class="uk-modal-close-outside" type="button" uk-close></button><div id="videos"><iframe class="SameSite" src="https://www.youtube.com/watch?v='+ videoLink +'" width="500" height="281" frameborder="0" uk-video></iframe></div></div></div>'
+  document.querySelector('#videosTodos').innerHTML += '<a class="uk-button uk-button-default" href="#modal-media-youtube" uk-toggle><p id="videoNombre">'+ videoNombre +'</p></a><div id="modal-media-youtube" class="uk-flex-top" uk-modal><div class="uk-modal-dialog uk-width-auto uk-margin-auto-vertical"><button class="uk-modal-close-outside" type="button" uk-close></button><div id="videos"><iframe class="SameSite" src="https://www.youtube.com/embed/'+ videoLink +'" width="500" height="281" frameborder="0" uk-video></iframe></div></div></div>'
 }
 })
+
+// Favoritos2
+
+document.getElementById("agregar-favoritos").addEventListener("click", function(e) {
+
+  // hacemos que no se ejecute el enlace
+  e.preventDefault();
+  fetch("https://api.themoviedb.org/3/tv/"+ idSerie +"?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US").then(res => res.json())
+  .then(data => {
+    var nombre = data.name;
+    var datosFav = {
+    id: idSerie,
+    nombre: nombre,
+  };
+  var posFav = JSON.parse(window.localStorage.getItem('fav'))
+  console.log(posFav);
+  if (posFav == null) {
+    document.querySelector('#textoFavoritos').innerHTML = "Agregar a favoritos"
+    document.getElementById("agregar-favoritos").addEventListener("click", function add(){
+    window.localStorage.setItem('fav', JSON.stringify(datosFav))
+  })
+    console.log(window.localStorage.getItem('fav'));
+  }else {
+    document.querySelector('#textoFavoritos').innerHTML = "Eliminar de favoritos"
+    document.getElementById("agregar-favoritos").addEventListener("click", function remove(){
+      window.localStorage.removeItem('fav' , datosFav)
+    })
+}
+})
+})
+// Favoritos2
+
+//relacionados
+fetch("https://api.themoviedb.org/3/tv/"+ idSerie +"/recommendations?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US&page=1")
+.then(res => res.json())
+.then(data => {
+  console.log(data.results);
+  var relacionados = data.results;
+  var prepath = "https://image.tmdb.org/t/p/original"
+  var ulrelacionados = document.getElementById("relacionados");
+  var lirelacionados = ""
+ for (var i = 0; i < relacionados.length; i++) {
+   var nombre = relacionados[i].name
+   var path = relacionados[i].poster_path
+   var overview = relacionados[i].overview.substring(0,100)
+   var valoracion = relacionados[i].vote_average
+    lirelacionados = '<a href="detalle.html?id='+ relacionados[i].id +'"><li>'
+    lirelacionados +=     '<div class="uk-card uk-card-default ">'
+    lirelacionados +=         '<div class="uk-card-media-top">'
+    lirelacionados +=             '<img class="relacionados" src='+ prepath + path +' alt="">'
+    lirelacionados +=         '</div>'
+    lirelacionados +=         '<div class="uk-card-body">'
+    lirelacionados +=             '<h3 class="uk-card-title">'+  nombre +'</h3>'
+    lirelacionados +=             '<p class="relacionados">'+ overview + '...' +'</p>'
+    lirelacionados +=             '<p>Valoracion: '+ valoracion +'</p>'
+    lirelacionados +=         '</div>'
+    lirelacionados +=     '</div>'
+    lirelacionados += '</li></a>';
+    console.log(lirelacionados);
+    ulrelacionados.innerHTML += lirelacionados;
+}
+})
+//relacionados
 
 }
---
+++ valoradas.js
@@ -0,0 +1,36 @@
+window.onload = function(){
+  //Header
+  fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US")
+  .then(res => res.json())
+  .then(data => {
+  console.log(data);
+  var generos = data.genres
+  for (var i = 0; i < generos.length; i++) {
+    var nombre = generos[i].name
+    var idGenero = generos[i].id
+    document.querySelector('#listaGeneros').innerHTML += '<li><a href=generos.html?id="'+ idGenero +'">'+ nombre +'</a></li>'
+  }
+    })
+    //header
+
+// lista
+fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US&page=1")
+.then(res => res.json())
+.then(data => {
+  console.log(data.results);
+  var valoradas = data.results;
+  var prepath = "https://image.tmdb.org/t/p/original"
+  var trvaloradas = ""
+ for (var i = 0; i < valoradas.length; i++) {
+   var nombre = valoradas[i].name
+   var path = valoradas[i].poster_path
+   var overview = valoradas[i].overview.substring(0,100)
+   var valoracion = valoradas[i].vote_average
+
+   trvaloradas = '<tr>'
+   trvaloradas += '<td><img class="uk-preserve-width uk-border-circle" src="'+prepath+path+'" width="40" alt=""></td>                                                   <td class="uk-text-truncate">'+nombre+'</td>                                                 <td class="uk-table-link">                          <a class="uk-link-reset" href="detalle.html?id='+ valoradas[i].id+'">'+overview+'</a></td>                                                <td class="uk-text-nowrap">'+valoracion+'</td></tr>'
+    console.log(trvaloradas);
+    document.getElementById("tbvaloradas").innerHTML += trvaloradas;
+}
+})
+}

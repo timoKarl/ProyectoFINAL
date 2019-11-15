14211d8f918a3531c068ced4545e5b2c5d6fdc12
brunotojeiro <btojeiro@gmail.com>
2019-11-15 19:55:02 -0300

    probando inf scroll y reparando valoradas y login

--- valoradas.js
+++ valoradas.js
@@ -3,7 +3,7 @@ window.onload = function(){
   fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US")
   .then(res => res.json())
   .then(data => {
-  console.log(data);
+  // console.log(data);
   var generos = data.genres
   for (var i = 0; i < generos.length; i++) {
     var nombre = generos[i].name
@@ -15,34 +15,88 @@ window.onload = function(){
 
 // lista0
 
-fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US")
-.then(res => res.json())
-.then(data => {
-  console.log(data);
-  var cantidadDePaginas = data.total_pages;
 
-for (var i = 1; i < cantidadDePaginas; i++) {
-  fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US&page="+i)
+/*fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US")
   .then(res => res.json())
   .then(data => {
-      console.log(data);
-      var valoradas = data.results;
-      var prepath = "https://image.tmdb.org/t/p/original"
-      var trvaloradas = ""
-      for (var i = 0; i < valoradas.length; i++) {
-       var nombre = valoradas[i].name
-       var path = valoradas[i].poster_path
-       var overview = valoradas[i].overview
-       var valoracion = valoradas[i].vote_average
+    console.log(data);
+    var valoradas = data.results;
+          var prepath = "https://image.tmdb.org/t/p/original"
+          var trvaloradas = ""
+          for (var i = 0; i < 21; i++) {
+           var nombre = valoradas[i].name
+           var path = valoradas[i].poster_path
+           var overview = valoradas[i].overview
+           var valoracion = valoradas[i].vote_average
 
-       trvaloradas = '<tr>'
-       trvaloradas += '<td><img class="uk-preserve-width uk-border-circle lazyload" src="'+prepath+path+'" width="40" alt=""></td>                                                   <td class="uk-text-truncate">'+nombre+'</td>                                                 <td class="uk-table-link">                          <a class="uk-link-reset" href="detalle.html?id='+ valoradas[i].id+'">'+overview+'</a></td>                                                <td class="uk-text-nowrap">'+valoracion+'</td></tr>'
-        console.log(trvaloradas);
-        document.getElementById("tbvaloradas").innerHTML += trvaloradas;
-      }
+           trvaloradas = '<tr>'
+           trvaloradas += '<td><img class="uk-preserve-width uk-border-circle lazyload" src="'+prepath+path+'" width="40" alt=""></td>                                                   <td class="uk-text-truncate">'+nombre+'</td>                                                 <td class="uk-a-link">                          <a class="uk-link-reset" href="detalle.html?id='+ valoradas[i].id+'">'+overview+'</a></td>                                                <td class="uk-text-nowrap">'+valoracion+'</td></tr>'
+            console.log(trvaloradas);
+            document.getElementById("tbvaloradas").innerHTML += trvaloradas;
+
+          }
+  })*/
+  .catch(error => {
+    console.log(error);
+
   })
+
+
+
+
+
+
+ fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US")
+ .then(res => res.json())
+ .then(data => {
+   console.log(data);
+   var cantidadDePaginas = data.total_pages;
+
+ for (var i = 1; i < 2; i++) {
+   fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US&page=1")
+   .then(res => res.json())
+   .then(data => {
+       console.log(data);
+       var valoradas = data.results;
+       var prepath = "https://image.tmdb.org/t/p/original"
+       var trvaloradas = ""
+       for (var i = 0; i < 21; i++) {
+        var nombre = valoradas[i].name
+        var path = valoradas[i].poster_path
+        var overview = valoradas[i].overview
+        var valoracion = valoradas[i].vote_average
+//
+        trvaloradas = '<tr>'
+        trvaloradas += '<td><img class="uk-preserve-width uk-border-circle lazyload" src="'+prepath+path+'" width="40" alt=""></td>                                                   <td class="uk-text-truncate">'+nombre+'</td>                                                 <td class="uk-a-link">                          <a class="uk-link-reset" href="detalle.html?id='+ valoradas[i].id+'">'+overview+'</a></td>                                                <td class="uk-text-nowrap">'+valoracion+'</td></tr>'
+         console.log(trvaloradas);
+         document.getElementById("tbvaloradas").innerHTML += trvaloradas;
+
+       }
+   })
+   }
+//
+ })
+/*onScroll () {
+  var nearBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - 100;
+  if (!this.props.fetching && nearBottom) {
+    fetchItems();  // dispatch action
   }
+}
 
-})
+render () {
+  var items = this.props.items.map(item => (<Component {item}/>)
 
+  return (
+    <ul>{ items }</ul>
+  )
+}
+
+componentDidMount() {
+  window.addEventListener("scroll", this.onScroll.bind(this), false);
+}
+componentWillUnmount() {
+  window.removeEventListener("scroll", this.onScroll.bind(this), false);
+}
+
+onScroll()*/
 }

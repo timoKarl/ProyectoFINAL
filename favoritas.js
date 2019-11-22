af9781e7265d44abdbc91fa8e6e07f6093343ddd
ian <ijamui@udesa.edu.ar>
2019-11-22 00:05:23 -0300

    menu mobile tablet

--- favoritas.js
+++ favoritas.js
@@ -33,24 +33,24 @@ fetch("https://api.themoviedb.org/3/tv/"+fav[i]+"?api_key=87b4351691f0835cf822a9
 
 
        trfavoritas = '<tr>'
-       trfavoritas += '<td><img class="uk-preserve-width uk-border-circle" src="'+prepath+path+'" width="40" alt=""></td><td class="uk-text-truncate">'+ nombre +'</td><td class="uk-table-link"><a class="uk-link-reset" href="detalle.html?id='+ idSerie +'">'+overview+'</a></td><td id="idSerie'+i+'" class="uk-text-nowrap">'+idSerie+'</td><td class="uk-text-nowrap"><p id="textoFavoritos'+i+'">Quitar</p></td></tr>'
+       trfavoritas += '<td><img class="uk-preserve-width uk-border-circle" src="'+prepath+path+'" width="40" alt=""></td><td class="uk-text-truncate">'+ nombre +'</td><td class="uk-table-link"><a class="uk-link-reset" href="detalle.html?id='+ idSerie +'">'+overview+'</a></td><td id="idSerie'+i+'" class="uk-text-nowrap">'+valoracion+'/10</td></tr>'
         document.getElementById("tbfavoritas").innerHTML += trfavoritas;
 
 
-        var fav = JSON.parse(window.localStorage.getItem('fav'))
-        for (var i = 0; i < fav.length; i++) {
-          // Agregar/eliminar
-          document.getElementById("textoFavoritos"+i).addEventListener("click",  function eliminarDeFav(){
-            // la serie eszta en fav. la quito
-            console.log(fav);
-            var idSerie = document.getElementById('idSerie'+i)
-            var index = fav.indexOf(idSerie);
-            fav.splice(index, 1);
-            window.localStorage.setItem('fav' , JSON.stringify(fav))
-            console.log("elimino de fav");
-          })
-
-}
+//         var fav = JSON.parse(window.localStorage.getItem('fav'))
+//         for (var i = 0; i < fav.length; i++) {
+//           // Agregar/eliminar
+//           document.getElementById("textoFavoritos"+i).addEventListener("click",  function eliminarDeFav(){
+//             // la serie eszta en fav. la quito
+//             console.log(fav);
+//             var idSerie = document.getElementById('idSerie'+i)
+//             var index = fav.indexOf(idSerie);
+//             fav.splice(index, 1);
+//             window.localStorage.setItem('fav' , JSON.stringify(fav))
+//             console.log("elimino de fav");
+//           })
+//
+// }
 })
 }
   //Agregar/eliminar

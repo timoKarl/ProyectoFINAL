b26335fe32b68d3635c8f09c8e6305292acb9e18
ian <ijamui@udesa.edu.ar>
2019-11-22 17:05:09 -0300

    bhjjjbkbkj

--- buscadorAvanzado.js
+++ buscadorAvanzado.js
@@ -13,21 +13,53 @@ for (var i = 0; i < generos.length; i++) {
 for (var i = 0; i < generos.length; i++) {
   var nombre = generos[i].name
   var idGenero = generos[i].id
-  document.querySelector('#listaGenerosCon').innerHTML += '<option>'+ nombre +'</option>'
+  document.querySelector('#listaGenerosCon').innerHTML += '<option value='+idGenero+'>'+ nombre +'</option>'
 }
 for (var i = 0; i < generos.length; i++) {
   var nombre = generos[i].name
   var idGenero = generos[i].id
-  document.querySelector('#listaGenerosSin').innerHTML += '<option>'+ nombre +'</option>'
+  document.querySelector('#listaGenerosSin').innerHTML += '<option value='+idGenero+'>'+ nombre +'</option>'
 }
-  })
-  //header
+})
+//header
 
-  var query = new URLSearchParams(location.search)
+var query = new URLSearchParams(location.search)
 
 var orden = ""
+if (query.has('orden')){
+  var orden = query.get('orden')
+  if (orden=="PopularidadDescendente") {
+    orden = "popularity.desc"
+  }else {
+    if (orden=="PopularidadAscendente") {
+      orden = "popularity.asc"
+    }else {
+      if (orden=="ValoracionDescendente") {
+        orden = "vote_average.desc"
+      }else {
+        if (orden=="ValoracionAscendente") {
+          orden = "vote_average.asc"
+        }else {
+          if (orden=="fechaDescendente") {
+            orden = "first_air_date.desc"
+          }else {
+            if (orden=="fechaAscendente") {
+              orden = "first_air_date.asc"
+            }
+          }
+        }
+      }
+    }
+  }
+  }
+
 var estreno = ""
+if (query.has('estreno')){
+  var estreno = query.get('estreno')
+  }
+
 var conGenero = ""
+var preConGenero = ""
 if (query.has('conGenero')){
   var conGenero1 = query.get('conGenero')
   if (conGenero1 != "genero") {
@@ -46,25 +78,17 @@ if (query.has('sinGenero')){
   }
 }
 
-var preKeyWords = ""
-var keyWords = ""
-if (query.has('keyWords')){
-  preKeyWords = "&with_keywords="
-  var keyWords = query.get('keyWords')
-}
-fetch("https://api.themoviedb.org/3/discover/tv?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US&sort_by="+orden+"&first_air_date_year="+estreno+"&page=1&with_genres="+conGenero+"&without_genres="+sinGenero+"&include_null_first_air_dates=fals&with_keywords="+keyWords)
-
 document.getElementById("mensaje").style.display = "none";
 document.getElementById("lista").style.display = "none";
-var url = "https://api.themoviedb.org/3/discover/tv?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US&sort_by="+orden+"&air_date.gte="+estreno+"&page=1&timezone=America%2FNew_York"+preConGenero+conGenero+preSinGenero+sinGenero+"&include_null_first_air_dates=false&with_keywords="+keyWords
+var url = "https://api.themoviedb.org/3/discover/tv?api_key=87b4351691f0835cf822a9ad51618e50&language=en-US&sort_by="+orden+"&first_air_date_year="+estreno+"&page=1&timezone=America%2FNew_York"+preConGenero+conGenero+preSinGenero+sinGenero
 console.log(url);
 fetch(url)
 .then(res => res.json())
 .then(data => {
-// if (conGenero != "genero" && sinGenero != "genero") {
-// document.getElementById("mensaje").style.display = "block";
-// document.getElementById("mensaje").innerHTML +="<h2>Solo puedes seleccionar un genero a la vez</h2>";
-// }else {
+if (conGenero != "" && sinGenero != "") {
+document.getElementById("mensaje").style.display = "block";
+document.getElementById("mensaje").innerHTML +="<h2>Solo puedes seleccionar un genero a la vez</h2>";
+}else {
   console.log(data.results);
   document.getElementById("lista").style.display = "block";
   var resultados1 = data.results;
@@ -81,9 +105,10 @@ fetch(url)
      var valoracion = resultados1[i].vote_average
 
      trresultados1 = '<tr>'
-     trresultados1 += '<td><img class="uk-preserve-width uk-border-circle" src="'+prepath+path+'" width="40" alt=""></td>                                                   <td class="uk-text-truncate">'+nombre+'</td>                                                 <td class="uk-table-link">                          <a class="uk-link-reset" href="detalle.html?id='+ resultados1[i].id+'">'+overview+'</a></td>                                                <td class="uk-text-nowrap">'+valoracion+'</td></tr>'
+     trresultados1 += '<td><img class="uk-preserve-width uk-border-circle" src="'+prepath+path+'" width="40" alt=""></td>                                                                        <td class="uk-text-truncate">'+nombre+'</td>                                                <td class="uk-table-link">                                                                    <a class="uk-link-reset" href="detalle.html?id='+ resultados1[i].id+'">'+overview+'</a></td> <td class="uk-text-nowrap">'+valoracion+'/10</td></tr>'
 
     document.getElementById("tbresultados1").innerHTML += trresultados1;
 }
-// }
+}
 })
+}
